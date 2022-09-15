7ed79ad65f71cd4e2a5763ce56aa7b844c9687d7
ezgidrmz <ezgidurmaz999@gmail.com>
2022-09-16 01:27:32 +0300

    java2022
    javademos

---
+++ javademos/variables/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String mesaj= "Öğrenci sayısı:";
+        int ogrenciSayisi = 12;
+        System.out.println(mesaj+ ogrenciSayisi);
+        System.out.println(mesaj+ ogrenciSayisi);
+        System.out.println("Öğrenci Sayısı:"+ ogrenciSayisi);
+        System.out.println("Öğrenci Sayısı:"+ ogrenciSayisi);
+        System.out.println("Öğrenci Sayısı:"+ ogrenciSayisi);
+
+
+
+    }
+}
